cd4b49587bf91c58fa2b91f9016a24462ff88444
gd-codes <45459312+gd-codes@users.noreply.github.com>
2021-06-24 09:41:46 +0530

    Merged develop/wip-colour-palette into main
    Work on updated website is complete, Version 2.0.0

--- scripts/functionWriter.js
+++ scripts/functionWriter.js
@@ -1,3 +1,33 @@
+const colourlist = [[220,220,220],[180,180,180],[255,255,255],[132,132,132],[108,108,108],[153,153,153],[65,65,65],
+      [53,53,53],[75,75,75],[22,22,22],[18,18,18],[26,26,26],[88,65,44],[72,53,36],[102,75,51],[132,44,44],
+      [108,36,36],[153,51,51],[186,108,44],[152,88,36],[216,125,51],[198,198,44],[162,162,36],[230,230,51],[108,176,22],
+      [88,144,18],[125,204,26],[88,108,44],[72,88,36],[102,125,51],[66,108,132],[54,88,108],[77,125,153],[88,132,186],
+      [72,108,152],[102,153,216],[44,66,152],[36,54,124],[51,77,176],[108,55,152],[88,45,124],[125,64,176],[152,66,186],
+      [124,54,152],[176,77,216],[208,108,142],[170,88,116],[241,125,165],[124,100,60],[101,82,49],[144,116,70],[112,74,42],
+      [92,61,34],[130,86,49],[128,54,84],[105,44,69],[148,63,97],[50,122,120],[41,100,98],[58,141,139],[130,94,66],
+      [106,77,54],[151,109,77],[212,200,140],[173,164,115],[246,232,162],[140,144,158],[115,118,129],[162,167,183],[96,96,96],
+      [79,79,79],[111,111,111],[86,86,86],[70,70,70],[100,100,100],[96,0,0],[79,0,0],[111,0,0],[220,216,210],[180,177,172],
+      [255,250,243],[116,92,84],[95,75,69],[134,107,97],[18,108,116],[15,88,95],[21,125,134],[0,108,0],[0,88,0],[0,125,0],
+      [55,80,20],[45,65,16],[64,93,23],[60,78,38],[49,64,31],[70,90,44],[45,70,45],[37,57,37],[52,81,52],
+      [108,144,128],[88,118,105],[125,167,148],[80,20,25],[65,16,20],[93,23,29],[75,37,52],[61,30,43],
+      [87,43,60],[162,42,42],[133,34,34],[188,49,49],[15,155,115],[12,127,94],[17,180,133],[78,188,182],[64,154,149],
+      [90,218,211],[144,144,144],[118,118,118],[167,167,167],[220,0,0],[180,0,0],[255,0,0],[215,205,65],[176,168,53],
+      [249,238,75],[0,188,50],[0,154,41],[0,218,58],[64,110,220],[52,90,180],[74,128,255],[185,150,125],[151,123,102],
+      [214,174,145],[180,150,140],[147,123,115],[209,174,162],[50,35,30],[41,29,25],[58,41,35],[65,42,30],[53,34,25],
+      [75,49,35],[108,152,48],[88,124,39],[125,176,56],[170,170,170],[139,139,139],[197,197,197],[138,138,220],[113,113,180],
+      [160,160,255],[125,160,75],[102,131,61],[145,185,87]];
+/*Do NOT change these values. The colours are the same as pixels will be, based on `colourmap` of `imageProcessor.js`
+and each colour type appears thrice consecutively in colourlist - regular, dark variant, light variant 
+The names in `blocks` are ordered to match colourlist.*/
+const blocks = ["concrete 0","concrete 8","concrete 7","concrete 15","concrete 12","concrete 14","concrete 1",
+      "concrete 4","concrete 5","concrete 13","concrete 9","concrete 3","concrete 11","concrete 10","concrete 2",
+      "concrete 6","planks 0","planks 1","crimson_planks 0","warped_planks 0","dirt 1","sandstone 0","clay 0",
+      "stone 0","deepslate 0","netherrack 0","quartz_block 0","waxed_exposed_copper 0","waxed_oxidized_copper 0", 
+      "azalea_leaves 1","leaves 12","leaves 14","leaves 13","glow_lichen 0","crimson_hyphae 0",
+      "warped_hyphae 0","crimson_nylium 0","warped_wart_block 0","diamond_block 0","iron_block 0","redstone_block 0",
+      "gold_block 0","emerald_block 0","lapis_block 0","raw_iron_block 0","calcite 0","tuff 0","dripstone_block 0",
+      "slime 0","web 0","blue_ice 0","grass 0"];
+
 function getPixelAt(x, z, dataobj) {
   var i = 4*(dataobj.width*(z) + x);
   return [dataobj.data[i], dataobj.data[i+1], dataobj.data[i+2]];
@@ -16,24 +46,23 @@ function findYMap(imgdata, maxY) {
   for (x=0; x<imgdata.width; x++) {
     column = [0];
     for (z=0; z<imgdata.height; z++) {
-      c = indexOf(getPixelAt(x,z,imgdata), all_colours);
-      type = Math.floor(c/base_colours.length);
-      // type 0 if it is in base colours, 1 if brighter, 2 if darker shade
-      //Block must be at same height; higher; lower; than the one N of it
+      c = indexOf(getPixelAt(x,z,imgdata), colourlist);
+      type = c % 3;
+      // type 0 if it is in base colours, 1 if darker, 2 if brighter shade
+      //Block must be at same height; lower; higher; than the one N of it
       lastY = column[column.length - 1];
       switch (type) {
         case 0: 
           column.push(lastY);
           break;
         case 1:
-          column.push(lastY + 2);
-          break;
-        case 2:
-          if (lastY < 2) {
-            //Blocks can't go lower than base height
+          if (lastY < 2) { //Blocks can't go lower than base height
             column = column.map(a => a+2);
           }
           column.push(lastY);
+          break;
+        case 2:
+          column.push(lastY + 2);
           break;
       }
     }
@@ -45,24 +74,24 @@ function findYMap(imgdata, maxY) {
 }
 
 function writeCommands(name, imobj, palette, extrainfo, keep, linkpos) {
-  var zone_origins=[], x0, z0, i, fnlist=[], yMap, ymax=0;
+  var zone_origins=[], x0, z0, i, fnlist=[], yMap, ymax=1;
   //Divide image area into 64x128 zones for individual functions (8164 pixels per zone)
   for (z0=0; z0<imobj.height; z0+=128) {
     for (x0=0; x0<imobj.width; x0+=64) {
       zone_origins.push([x0,z0]);
     }
-  }
-  if (palette=="extended") {
+  } 
+  if (extrainfo > 1) {
     ymax = extrainfo;             //If extended, extrainfo contains the max height
     yMap = findYMap(imobj, ymax); // Get the (y) heights if it is a 3D arrangement
   }
   for (i=0; i<zone_origins.length; i++) {
-    var fun="", x, z, xloop, zloop, pix, code, replMode, yMap;
+    var fun="", x, y, z, xloop, zloop, pix, code, replMode;
     x0 = zone_origins[i][0]; z0 = zone_origins[i][1];
     fun += "say Running commands ".concat((i+1), "/", zone_origins.length, 
             " of function group ", name, " (x=", x0, "-", x0+63, ", z=",
             z0, "-", z0+127, ")\nsay Common coords : ", linkpos,
-            " | Do not destroy : ", keep, " | Colours : ", palette, 
+            " | Do not destroy : ", keep, " | Colours : ", palette.length, 
             " | Height : ", ymax, "\n");
     /* If positions are linked, coordinates for each zone have same origin (image's top left)
     Else iterate from (0,0) each time - user will have to shift by x=64/z=128 for each zone*/
@@ -78,26 +107,20 @@ function writeCommands(name, imobj, palette, extrainfo, keep, linkpos) {
     for (x=xloop; x < xloop+64; x++) {
       for (z=zloop; z < zloop+128; z++) {
         pix = (linkpos)? getPixelAt(x,z,imobj) : getPixelAt(x+x0,z+z0,imobj);
-        switch (palette) {
-          case "basic" : //2d
-            code = indexOf(pix, dye_colours);
-            fun += "setblock ~".concat(x, " ~ ~", z, " ", extrainfo, " ",
-                  code, replMode); // If basic, extrainfo contains the material name
-            break; 
-          case "standard" : //2d
-            code = base_blocks[indexOf(pix, base_colours)];
-            fun += "setblock ~".concat(x, " ~ ~", z, " ", code, replMode);
+        code = blocks[Math.floor(indexOf(pix, colourlist) / 3)];
+        y = (extrainfo <= 1)? 0 : yMap[x][z];
+        switch (code) {
+            // Anomalies - some blocks must be loaded as structures
+          case "azalea_leaves 1":
+            fun += ((keep)?`execute @p ~ ~ ~ detect ~${x} ~${y} ~${z} air -1 `:"") + 
+              `structure load mapart:azalea_leaves ~${x} ~${y} ~${z}\n`;
             break;
-          case "extended" : //3d
-            code = base_blocks[indexOf(pix, all_colours) % base_colours.length];
-            if (linkpos) {
-              fun += "setblock ~".concat(x, " ~", yMap[x][z], " ~", z, " ", code,
-                                        replMode);
-            } else {
-              fun += "setblock ~".concat(x, " ~", yMap[x+x0][z+z0], " ~", z, " ",
-                                         code, replMode);
-            }
+          case "glow_lichen 0":
+            fun += ((keep)?`execute @p ~ ~ ~ detect ~${x} ~${y-1} ~${z} air -1 `:"") + 
+              `structure load mapart:glow_lichen ~${x} ~${y-1} ~${z}\n`;
             break;
+          default : // Normal case, direct placement for most of the blocks
+            fun += `setblock ~${x} ~${y} ~${z} ${code}${replMode}`;
         }
       }
     }
--- scripts/imageProcessor.js
+++ scripts/imageProcessor.js
@@ -1,42 +1,29 @@
 
-const dye_colours = [
-  [218,218,218], [175,112,55], [140,72,175], [95,130,180],
-  [195,195,75], [125,175,56], [195,115,135], [64,64,64],
-  [128,128,128], [64,108,128], [108,53,150], [42,64,150],
-  [88,64,42], [88,108,42], [128,42,42], [20,20,20]
-]; 
 //None of these values should be > 220
-const base_colours = dye_colours.concat([
-  [218,218,205], [136,136,218], [96,96,96], [128,96,64],
-  [120,100,60], [54,78,21], [45,70,45], [96,0,0],
-  [218,0,0], [218,128,0], [0,104,0], [218,160,80],
-  [120,70,42], [78,185,180], [64,108,218], [0,184,48],
-  [0,0,0], [146,28,28], [16,128,112], [75,58,48],
-  [208,208,64], [169,169,169], [208,198,138], [139,144,156],
-  [108,150,48], [64,108,128]
-]);
-const all_colours = base_colours.concat(
-  base_colours.map(function(c){
-    return [Math.round(255/220*c[0]), Math.round(255/220*c[1]), Math.round(255/220*c[2])]}),
-  base_colours.map(function(c){
-    return [Math.round(180/220*c[0]), Math.round(180/220*c[1]), Math.round(180/220*c[2])]})
-); //Don't change the indices/order of any of these lists
-const base_blocks = [
-  "concrete 0", "concrete 1", "concrete 2", "concrete 3",
-  "concrete 4", "concrete 5", "concrete 6", "concrete 7", 
-  "concrete 8", "concrete 9", "concrete 10", "concrete 11", 
-  "concrete 12", "concrete 13", "concrete 14", "concrete 15",
-  "quartz_block 0", "blue_ice 0", "stone 0", " dirt 1",
-  "crafting_table 0", "leaves 12", "leaves 13", "netherrack 0",
-  "redstone_block 0", "shroomlight 0", "dried_kelp_block 0", "glowstone 0",
-  "redstone_lamp 0", "diamond_block 0", "lapis_block 0", "emerald_block 0",
-  "netherite_block 0", "crimson_nylium 0", "warped_nylium 0", "soul_soil 0",
-  "gold_block 0", "web 0", "end_stone 0", "iron_block 0",
-  "slime 0", "prismarine 0"
-];
+const colourmap = {white: [220, 220, 220], lightgrey: [132, 132, 132], grey: [65, 65, 65], black: [22, 22, 22], 
+                   brown: [88, 65, 44], red: [132, 44, 44], orange: [186, 108, 44], yellow: [198, 198, 44], 
+                   lime: [108, 176, 22], green: [88, 108, 44], cyan: [66, 108, 132], lightblue: [88, 132, 186], 
+                   blue: [44, 66, 152], purple: [108, 55, 152], magenta: [152, 66, 186], pink: [208, 108, 142], 
+                   oak: [124, 100, 60], spruce: [112, 74, 42], crimson: [128, 54, 84], warped: [50, 122, 120], 
+                   dirt: [130, 94, 66], sand: [212, 200, 140], clay: [140, 144, 158], stone: [96, 96, 96], 
+                   deepslate: [86, 86, 86], nether: [96, 0, 0], quartz: [220, 216, 210], expocopper: [116, 92, 84], 
+                   oxicopper: [18, 108, 116], foliage: [0, 108, 0], oakleaves: [55, 80, 20], birchleaves: [60, 78, 38], 
+                   conifers: [45, 70, 45], lichen: [108, 144, 128], darkcrimson: [80, 20, 25], 
+                   darkwarped: [75, 37, 52], crimsonylium: [162, 42, 42], warpwart: [15, 155, 115], 
+                   turquoise: [78, 188, 182], steel: [144, 144, 144], brightred: [220, 0, 0], gold: [215, 205, 65], 
+                   emerald: [0, 188, 50], lapis: [64, 110, 220], rawiron: [185, 150, 125], calcite: [180, 150, 140], 
+                   tuff: [50, 35, 30], dripstone: [65, 42, 30], slime: [108, 152, 48], web: [170, 170, 170], 
+                   ice: [138, 138, 220], grass: [125, 160, 75]}
+
+function lightPixel(rgb) {
+  return [Math.round(255/220*rgb[0]), Math.round(255/220*rgb[1]), Math.round(255/220*rgb[2])];
+}
+function darkPixel(rgb) {
+  return [Math.round(180/220*rgb[0]), Math.round(180/220*rgb[1]), Math.round(180/220*rgb[2])];
+}
 
 
-function analyseImage(uid, image, area, palette, dither) {
+function analyseImage(uid, image, area, palette, d3, dither) {
   //Manage the display etc
   var canv = $("#testCanvas")[0];
   var ctx = canv.getContext('2d');
@@ -52,26 +39,24 @@ function analyseImage(uid, image, area, palette, dither) {
     default:
       dispScale = 1;      
   }
+  var p = [];
+  for (var cn of palette.split(" ")) {
+    if (colourmap[cn] !== undefined) {
+      var rgb = (colourmap[cn]); p.push(rgb);
+      if (d3) {
+        p.push(darkPixel(rgb)); p.push(lightPixel(rgb));
+      }
+    }
+  }
   ctx.drawImage(image, 0, 0, image.width, image.height);
-  //var origCanvImg = ctx.getImageData(0,0,image.width,image.height);
   ctx.clearRect(0,0,canv.width,canv.height);
   canv.height = h; canv.width = w;
   ctx.drawImage(image, 0, 0, w, h);
   var imgData = ctx.getImageData(0,0,w,h);
   var resized_image = canv.toDataURL("image/png");
   ctx.clearRect(0, 0, w, h);
-  var finalImgData ;
-  switch (palette) {
-    case "basic" :
-      finalImgData = convertPalette(dye_colours, imgData, dither);
-      break;
-    case "standard" :
-      finalImgData = convertPalette(base_colours, imgData, dither);
-      break;
-    case "extended" :
-      finalImgData = convertPalette(all_colours, imgData, dither);
-      break;
-  }
+  
+  var finalImgData = convertPalette(p, imgData, dither);
   ctx.putImageData(finalImgData, 0, 0);
   var converted_image = canv.toDataURL("image/png");
   ctx.clearRect(0, 0, w, h);
@@ -168,5 +153,6 @@ function makeLogo(images) {
     return canv.toDataURL('image/png');
   } catch (err) {
     return "data:image/png;base64,";
+    console.error("Error forming a pack icon", err);
   } 
 }
